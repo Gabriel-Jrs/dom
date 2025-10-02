81214fa55ed7a36c8e412efb25a5180ceb38727e
Gabriel José <gabriell4924462@gmail.com>
2025-10-02 13:10:28 -0300

    Update q1.js

--- function/q1.js
+++ function/q1.js
@@ -1,20 +1,54 @@
 function valordVenda() {
-    let valor = []
-    let contador = 0
+    let vendas = []  
+    let contador = 0 
+    let totalVendas = 0 
     let vvenda = parseFloat(prompt("Digite quantas vendas você teve?"))
-    for (let i = 0; i < vvenda; i++) {
-        valor = parseFloat(prompt(`Digite o valor da venda ${i + 1}:`))
-        valor.push(vvenda)
-        contador++
-    }
-    vendas(contador)
-    return
-}
-function vendas(cont){
     
+    for (let i = 0; i < vvenda; i++) {
+        let valorVenda = parseFloat(prompt(`Digite o valor da venda ${i + 1}:`)) 
+        vendas.push(valorVenda)  
+        totalVendas += valorVenda  
+        contador++  
+    }
+
+    vendas(contador, totalVendas) 
 }
 
+function vendas(cont, total) {
+    if(contador==0){
+        console.log("NAO HA VENDAS!")
+        menu()
+    }
+    else{
+    console.log(`Total de vendas: ${cont}`)
+    console.log(`Valor total das vendas: R$ ${total.toFixed(2)}`)
+    }
+}
+function vdispesas(){
+    let valordes = []  
+    let contador = 0 
+    let totalDespesas = 0 
+    let despesas = parseFloat(prompt("Digite quantas vendas você teve?"))
+    
+    for (let i = 0; i < despesas; i++) {
+        let totalDespesas = parseFloat(prompt(`Digite o valor da despesa ${i + 1}:`)) 
+        vendas.push(valorVenda)  
+        totalDespesas += valordes 
+        contador++  
+    }
 
+    despesas(contador, totalDespesas)
+}
+function despesas(cont, total) {
+    if(contador==0){
+        console.log("NAO HA DESPESAS!")
+        menu()
+    }
+    else{
+    console.log(`Total de DESPESAS: ${cont}`)
+    console.log(`Valor total das DESPESAS: R$ ${total.toFixed(2)}`)
+    }
+}
 function desejo(esc) {
     switch (esc) {
         case "1":
@@ -24,7 +58,7 @@ function desejo(esc) {
             vendas(esc)
             break
         case "3":
-            dispesas(esc)
+            vdispesas(esc)
             break
         case "4":
             despesas(esc)
